25ae05cdc7f7b41b0bd3a47d46cccb4348b83daa
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-06-29 12:01:55 -0400

    agregamos el inheritedWidget en nuestro login bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:patron_bloc/src/bloc/providers.dart';
 import 'package:patron_bloc/src/page/HomaPage.dart';
 import 'package:patron_bloc/src/page/LoginPage.dart';
 
@@ -7,14 +8,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material app',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-        'home': (BuildContext context) => HomePage()
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material app',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage()
+        },
+      ),
     );
   }
 }
